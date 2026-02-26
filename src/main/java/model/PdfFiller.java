a9734128ca7fc80571df201429ef21d7ed803a4d
Maryevans96 <161616882+Maryevans96@users.noreply.github.com>
2026-02-26 12:25:37 +0100

    MODIFICHE ROTAZIONE SECONDA PAGINA E MISURE INSERIMENTI. TUTTO OKK.

--- src/main/java/model/PdfFiller.java
+++ src/main/java/model/PdfFiller.java
@@ -5,6 +5,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.*;
+import org.apache.pdfbox.util.Matrix;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,6 +30,20 @@ public class PdfFiller {
             PDPageContentStream contentStreamFirstPage = new PDPageContentStream(pdfDocument, firstPage, PDPageContentStream.AppendMode.APPEND, true, true);
             PDPageContentStream contentStreamSecondPage = new PDPageContentStream(pdfDocument, secondPage, PDPageContentStream.AppendMode.APPEND, true, true);
 
+            // --- GESTIONE ROTAZIONE (VERSIONE PER TESTO SOTTOSOPRA) ---
+            int rotation = secondPage.getRotation();
+            float width = secondPage.getMediaBox().getWidth();
+            float height = secondPage.getMediaBox().getHeight();
+
+            if (rotation == 90) {
+                // Se era 90 ed è sottosopra, proviamo a traslare sull'altezza invece che sulla larghezza
+                contentStreamSecondPage.transform(Matrix.getRotateInstance(Math.toRadians(90), width, 0));
+            } else if (rotation == 270) {
+                contentStreamSecondPage.transform(Matrix.getRotateInstance(Math.toRadians(-90), 0, height));
+            } else {
+                // Se anche con 0 gradi esce sottosopra, forziamo un ribaltamento
+                contentStreamSecondPage.transform(Matrix.getRotateInstance(Math.toRadians(180), width, height));
+            }
 
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
@@ -37,23 +52,23 @@ public class PdfFiller {
             switch (modelName) {
                 case "Enrico", "Congiu", "Francesco", "Cristoforo":
                     // Code for First Page
-                    addTextToPdf(contentStreamFirstPage, datiAllegato.getNumeroOds(), 470, 683, 10);
-                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getDataOds() != null) ? sdf.format(datiAllegato.getDataOds()) : "", 267, 697, 10);
-                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getScadenzaOds() != null) ? sdf.format(datiAllegato.getScadenzaOds()) : "", 267, 683, 10);
-                    addTextToPdf(contentStreamFirstPage, datiAllegato.getVia(), 220, 670, 10);
-                    addTextToPdf(contentStreamFirstPage, datiAllegato.getDanneggiante(), 165, 645, 10);
+                    addTextToPdf(contentStreamFirstPage, datiAllegato.getNumeroOds(), 482, 697, 10);
+                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getDataOds() != null) ? sdf.format(datiAllegato.getDataOds()) : "", 270, 712, 10);
+                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getScadenzaOds() != null) ? sdf.format(datiAllegato.getScadenzaOds()) : "", 270, 697, 10);
+                    addTextToPdf(contentStreamFirstPage, datiAllegato.getVia(), 222, 684, 10);
+                    addTextToPdf(contentStreamFirstPage, datiAllegato.getDanneggiante(), 165, 660, 10);
 
-                    addTextToPdf(contentStreamFirstPage, datiAllegato.getDescrizioneIntervento(), 195, 560, 10);
-                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getInizioLavori() != null) ? sdf.format(datiAllegato.getInizioLavori()) : "", 200, 223, 10);
-                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getFineLavori() != null) ? sdf.format(datiAllegato.getFineLavori()) : "", 470, 223, 10);
+                    addTextToPdf(contentStreamFirstPage, datiAllegato.getDescrizioneIntervento(), 80, 555, 10);
+                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getInizioLavori() != null) ? sdf.format(datiAllegato.getInizioLavori()) : "", 200, 214, 10);
+                    addTextToPdf(contentStreamFirstPage, (datiAllegato.getFineLavori() != null) ? sdf.format(datiAllegato.getFineLavori()) : "", 487, 214, 10);
 
                     // --- Code for Second Page ---
-                    addTextToPdf(contentStreamSecondPage, datiAllegato.getNumeroOds(), 60, 337, 10);
-                    addTextToPdf(contentStreamSecondPage, (datiAllegato.getDataOds() != null) ? sdf.format(datiAllegato.getDataOds()) : "", 125,337, 10);
-                    addTextToPdf(contentStreamSecondPage, datiAllegato.getVia(), 190,337,10);
-                    addTextToPdf(contentStreamSecondPage, datiAllegato.getDescrizioneIntervento(), 320,337,10);
-                    addTextToPdf(contentStreamSecondPage, (datiAllegato.getInizioLavori() != null) ? sdf.format(datiAllegato.getInizioLavori()) : "", 490, 277, 10); // After "INIZIATE il"
-                    addTextToPdf(contentStreamSecondPage, (datiAllegato.getFineLavori() != null) ? sdf.format(datiAllegato.getFineLavori()) : "", 490, 252, 10); // After "TERMINATE il"
+                    addTextToPdf(contentStreamSecondPage, datiAllegato.getNumeroOds(), 40, 335, 10);
+                    addTextToPdf(contentStreamSecondPage, (datiAllegato.getDataOds() != null) ? sdf.format(datiAllegato.getDataOds()) : "", 110,335, 10);
+                    addTextToPdf(contentStreamSecondPage, datiAllegato.getVia(), 175,335,10);
+                    addTextToPdf(contentStreamSecondPage, datiAllegato.getDescrizioneIntervento(), 318,335,10);
+                    addTextToPdf(contentStreamSecondPage, (datiAllegato.getInizioLavori() != null) ? sdf.format(datiAllegato.getInizioLavori()) : "", 490, 270, 10); // After "INIZIATE il"
+                    addTextToPdf(contentStreamSecondPage, (datiAllegato.getFineLavori() != null) ? sdf.format(datiAllegato.getFineLavori()) : "", 490, 245, 10); // After "TERMINATE il"
                     break;
                 default:
                     System.err.println("Unrecognized or unhandled PDF model: " + modelName);
